41fdbe1e708e663dfcf4ba4121d6800c38ee9139
hbnationit <hbnation.it@gmail.com>
2024-05-22 23:00:24 +0900

    메일 발송 기능을 gmail에서 oci mail delivery로 변경

--- src/main/java/app/hbnationit/apiserver/utils/MailUtil.java
+++ src/main/java/app/hbnationit/apiserver/utils/MailUtil.java
@@ -1,23 +1,41 @@
 package app.hbnationit.apiserver.utils;
 
 import io.netty.handler.codec.MessageAggregationException;
+import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Properties;
 
 @Component
 public class MailUtil {
     protected Log logger = LogFactory.getLog(getClass());
-    private final JavaMailSender mailSender;
+    private final Session mailSession;
 
-    public MailUtil(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+
+    public MailUtil() {
+        Properties props = System.getProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.port", 587);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+
+        this.mailSession = Session.getDefaultInstance(props);
     }
 
     public void send(String sender, String[] receivers, String subject, String body) {
@@ -25,19 +43,33 @@ public class MailUtil {
     }
 
     public void send(String sender, String senderName, String[] receivers, String subject, String body) {
-        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessage msg = new MimeMessage(mailSession);
+        Transport transport;
 
         try {
-            MimeMessageHelper messageBuilder = new MimeMessageHelper(message, false, "UTF-8");
-            messageBuilder.setFrom(sender, senderName);
-            messageBuilder.setTo(receivers);
-            messageBuilder.setSubject(subject);
-            messageBuilder.setText(body);
-        } catch (MessagingException | UnsupportedEncodingException ignore) {
+            msg.setFrom(new InternetAddress(sender, senderName));
+            msg.setRecipients(Message.RecipientType.TO, String.join(",", receivers));
+            msg.setSubject(subject);
+            msg.setContent(body,"text/html");
+
+            transport = mailSession.getTransport();
+        } catch (MessagingException | UnsupportedEncodingException e) {
             throw new MessageAggregationException();
         }
 
-        mailSender.send(message);
-        logger.debug("mail send complete.");
+        try {
+            transport.connect(host, username, password);
+            transport.sendMessage(msg, msg.getAllRecipients());
+
+            logger.debug("mail send complete.");
+        } catch (Exception ignore) {
+            throw new MessageAggregationException();
+        } finally {
+            try {
+                transport.close();
+            } catch (MessagingException e) {
+                throw new MessageAggregationException();
+            }
+        }
     }
 }
